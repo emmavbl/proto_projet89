4c2d35b3c68c9714ebecdc75d048832b3b4e870e
Vodable <vodableemma@gmail.com>
2020-11-17 21:51:10 +0100

    Plus de Souvenirs  !

--- src/Document.js
+++ src/Document.js
@@ -8,7 +8,7 @@ function Document({path, desc, format, themes, onClick}){
             <h1>{desc}</h1>
             {format === "jpg" ? 
                 <img src={'../souvenirs/' + path + '.jpg'} alt={desc}></img> 
-                : <p>{raw(`../public/souvenirs/${path}.txt`)}</p>
+                : <p>{raw(`../public/souvenirs/${path}.txt`).substr(0,500).concat("...")}</p>
             }
             <div className="themes">
                 {themes.map((themes, index) =>(
