c7a10753b04227a56c83dca324c5761930120379
evodable <vodableemma@gmail.com>
2020-12-16 14:32:10 +0100

    Random choose forking

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import data from './souvenirs.json'
 import Document from './Document.js'
 // import History from './History.js'
 import {React, Component} from 'react';
+import {CSSTransitionGroup} from 'react-transition-group'
 
 
 class App extends Component {
@@ -19,19 +20,22 @@ class App extends Component {
     
 
   getLinks(idMem){ 
-    var linkToReturn = [];
+    var linksToReturn = [];
     data.links.forEach(link => {
       if (link.source === idMem){
-        linkToReturn.push(link);
+        linksToReturn.push(link);
       }
     })
-    return linkToReturn;
+    console.log(linksToReturn);
+    return linksToReturn;
   }
 
   nextMemory = e =>{
-    const nextMem = this.state.links[0].target;
+    let linkIndex = Math.floor(Math.random()* this.state.links.length);
+    
+    const nextMem = this.state.links[linkIndex].target;
     const nextLink = this.getLinks(nextMem);
-    console.log(nextLink);
+    console.log(this);
 
     //change currentMemory and current Link
     this.setState({ 
@@ -43,14 +47,23 @@ class App extends Component {
 
   render() {
     const memory = data.nodes[this.state.currentMemory]
+    const memId = this.state.links[0].source
     return (
       <div className="App" onClick = {this.nextMemory}>
         <header className="App-header">
-          <Document 
-            path = {memory.path}
-            desc = {memory.name}
-            format = {memory.format}
-          />
+          <CSSTransitionGroup 
+            transitionName = "example"
+            transitionAppear = {false}
+            transitionLeave = {false}
+            timeout={1000}>
+            <Document 
+              key = {memId}
+              path = {memory.path}
+              desc = {memory.name}
+              format = {memory.format}
+            />
+          </CSSTransitionGroup>
+          
         </header>
       </div>
     );
